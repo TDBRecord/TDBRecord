40f49376890231ea4673cafbc79f9eef9989d1a0
onTDB <github@ontdb.com>
2023-01-23 16:31:03 +0900

    Version 1.1.0-pre11

--- TDBRecord/__main__.py
+++ TDBRecord/__main__.py
@@ -5,7 +5,7 @@ import logging
 import click
 import json
 
-__version__ = "1.1.0-pre10"
+__version__ = "1.1.0-pre11"
 
 @click.version_option(prog_name="TDBRecord", version=__version__)
 @click.group()
